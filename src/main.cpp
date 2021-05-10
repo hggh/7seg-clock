db420a874467dfaace50891e793fb21b543af040
Jonas Genannt <jonas@brachium-system.net>
2021-05-10 21:39:47 +0200

    Update code for 7seg display

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,25 @@
 #include "numbers.h"
 
 #define LED_COUNT 58
-#define PIN_RGB_LEDS 22
-#define PIN_WIFI_RESET 17
+#define PIN_RGB_LEDS 17
+#define PIN_WIFI_RESET 34
 #define HOSTNAME "clocki"
 
 CRGB leds[LED_COUNT];
-CRGB::HTMLColorCode led_color = CRGB::Red;
+CRGB led_color = CRGB::Blue;
+int led_brightness = 60;
+String html_color_code = "#0039e6";
+bool update_leds = false;
+
 WebServer server(80);
 WiFiManager wm;
 Bounce2::Button wifi_cfg_reset = Bounce2::Button();
 
-const long  gmt_offset_sec = -3600;
-const int   day_light_offset_sec = 3600;
+const long gmt_offset_sec = -3600;
+const int day_light_offset_sec = 3600;
+
+uint8_t time_hour = 255;
+uint8_t time_minute = 255;
 
 void write_number_to_leds(uint8_t offset, uint8_t number) {
   for (uint8_t i = 0; i < 14; i++) {
@@ -35,14 +42,43 @@ void write_number_to_leds(uint8_t offset, uint8_t number) {
   }
 }
 
+void clock_display_number(uint8_t offset, uint8_t number, boolean display_zero = false) {
+  uint8_t t_first = ((number / 10 ) % 10);
+  uint8_t t_second = (number % 10 );
+
+  if (display_zero == true || t_first != 0 ) {
+    write_number_to_leds(offset, t_first);
+  }
+  write_number_to_leds(offset + 14, t_second);
+}
+
+
 void webHandleRoot() {
   String content = "";
   File file = SPIFFS.open("/index.html");
   while(file.available()){
     content += char(file.read());
   }
+  content.replace("LED_BRIGHTNESS_VALUE", String(led_brightness));
+  content.replace("LED_SINGLE_COLOR_CODE", html_color_code);
   file.close();
   server.send(200, "text/html", content);
+}
+
+void webHandleUpdate() {
+  String req_color_single = server.arg("color");
+  html_color_code = req_color_single;
+  req_color_single.replace("#", "");
+
+  long color_code = strtol(req_color_single.c_str(), NULL, 16);
+  led_color = CRGB(color_code);
+
+  String req_brightness = server.arg("brightness");
+  led_brightness = req_brightness.toInt();
+  FastLED.setBrightness(led_brightness);
+
+  update_leds = true;
+  webHandleRoot();
 }
 
 void wm_config_mode_callback(WiFiManager *myWiFiManager) {
@@ -54,6 +90,7 @@ void wm_config_mode_callback(WiFiManager *myWiFiManager) {
 }
 
 void setup() {
+  btStop();
   WiFi.mode(WIFI_STA);
   WiFi.setHostname(HOSTNAME);
   Serial.begin(115200);
@@ -71,11 +108,14 @@ void setup() {
   FastLED.show();
 
   server.on("/", webHandleRoot);
+  server.on("/update", webHandleUpdate);
   server.begin();
 
   MDNS.begin(HOSTNAME);
   MDNS.addService("http", "tcp", 80);
   configTime(gmt_offset_sec, day_light_offset_sec, "0.de.pool.ntp.org", "1.de.pool.ntp.org", "2.de.pool.ntp.org");
+  setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1);
+  tzset();
 
   wifi_cfg_reset.attach(PIN_WIFI_RESET, INPUT_PULLUP);
   wifi_cfg_reset.interval(25);
@@ -94,4 +134,29 @@ void loop() {
     delay(500);
     ESP.restart();
   }
+
+  struct tm timedate;
+  getLocalTime(&timedate);
+
+  if (timedate.tm_min != time_minute || timedate.tm_hour != time_hour || update_leds == true) {
+    // time has changed, display it on the WS2812b LEDs
+    time_minute = timedate.tm_min;
+    time_hour = timedate.tm_hour;
+    FastLED.clear();
+
+    if (time_hour == 0) {
+      clock_display_number(0, time_hour, true);
+    }
+    else {
+      clock_display_number(0, time_hour, false);
+    }
+    clock_display_number(30, time_minute, true);
+
+    // dots
+    leds[28] = led_color;
+    leds[29] = led_color;
+
+    FastLED.show();
+  }
+  delay(1);
 }
